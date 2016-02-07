d9063c561c7acfbea763cd5a4733180fa9345677
Oskar Slyk <o.slyk3@gmail.com>
2016-02-07 12:37:58 +0000

    Added gradle files to the project files. Hopefully this will resolve the issues when checking out.

---
+++ app/src/main/java/com/example/spanishgrammarapp/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.spanishgrammarapp;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void learnActivity(View view){
+        Intent intent = new Intent(this, LearnActivity.class);
+        startActivity(intent);
+    }
+
+    public void practiceActivity(View view){
+        Intent intent = new Intent(this, PracticeActivity.class);
+        startActivity(intent);
+    }
+
+    public void reviseActivity(View view){
+        Intent intent = new Intent(this, ReviseActivity.class);
+        startActivity(intent);
+    }
+
+    public void resourcesActivity(View view){
+        Intent intent = new Intent(this, ResourcesActivity.class);
+        startActivity(intent);
+    }
+
+    public void glossaryActivity(View view){
+        Intent intent = new Intent(this, GlossaryActivity.class);
+        startActivity(intent);
+    }
+
+    public void credits(View view){
+        Dialog creditsDialog = new Dialog(this);
+        creditsDialog.setTitle("Credits");
+        TextView credits = new TextView(this);
+        credits.setText(R.string.creditsDialog);
+        creditsDialog.addContentView(credits,
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        creditsDialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
