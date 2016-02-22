44813d164a7d175f637d22119628c7ecfe91bf9a
Nitin Kapoor <nitin.kapoor@kcl.ac.uk>
2016-02-22 13:00:37 +0000

    added to manifest to allow user permission for account management

--- app/src/main/java/com/example/spanishgrammarapp/MainActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.spanishgrammarapp;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         UserProgress up1 = new UserProgress(this.getBaseContext());
         up1.saveProgress();
     }
+
 
     public void learnActivity(View view){
         Intent intent = new Intent(this, LearnActivity.class);
