6e0467a19c265577def423d84be80605dda48816
janaldous <jat.torres@gmail.com>
2016-03-03 22:47:15 +0000

    added Situational Video Activity

--- app/src/main/java/com/example/spanishgrammarapp/TopicActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/TopicActivity.java
@@ -3,21 +3,28 @@ package com.example.spanishgrammarapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class TopicActivity extends AppCompatActivity {
-    private String topic;
+    public final static String CURRENT_TOPIC = "CURRENT_TOPIC";
+    private String currentTopic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic);
 
-//        This is how we tell what topic we've entered.
-        Intent intent = getIntent();
-        String topic2 = intent.getStringExtra("TOPIC");
+        //        This is how we tell what topic we've entered
+        currentTopic = getIntent().getStringExtra("TOPIC");
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(topic2);
+        TextView textView = (TextView) findViewById(R.id.tv_topic);
+        textView.setText(currentTopic);
+    }
+
+    public void enterSituationalVideo(View view){
+        Intent intent = new Intent(this, SituationalVideoActivity.class);
+        intent.putExtra(CURRENT_TOPIC, currentTopic );
+        startActivity(intent);
     }
 }
