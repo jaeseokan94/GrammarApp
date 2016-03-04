85507742843c909786845aec446954fcc15848a2
janaldous <jat.torres@gmail.com>
2016-03-04 12:36:06 +0000

    SituaitnoalVideoActivty works, only tested on emulator, need to test on device for Cecilia's videos, since they are too big for the emulator to handle

--- app/src/main/java/com/example/spanishgrammarapp/SituationalVideoActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/SituationalVideoActivity.java
@@ -1,16 +1,35 @@
 package com.example.spanishgrammarapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 /**
  * Created by janaldoustorres on 03/03/2016.
  */
-public class SituationalVideoActivity extends AppCompatActivity {
+public class SituationalVideoActivity extends AppCompatActivity  {
+    private static final String urlCMS = "https://lang-it-up.herokuapp.com/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_situational_video);
 
+                //VideoView videoView = (VideoView) findViewById(R.id.vv_situational_video);
+
+            String uri = urlCMS + "media/FACE_PALM.mp4";
+            Uri vidUri = Uri.parse(uri);
+
+            VideoView video_player_view = (VideoView) findViewById(R.id.vv_situational_video);
+
+            video_player_view.setVideoURI(vidUri);
+            video_player_view.requestFocus();
+            video_player_view.start();
+
+            MediaController vidControl = new MediaController(this);
+            vidControl.setAnchorView(video_player_view);
+            video_player_view.setMediaController(vidControl);
     }
 }
