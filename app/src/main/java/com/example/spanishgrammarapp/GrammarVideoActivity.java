3945b4c1c26b066451e22c3ce6e159db8a64569b
janaldous <jat.torres@gmail.com>
2016-03-06 15:45:30 +0000

    changed title tv of GrammarVideoActivity

--- app/src/main/java/com/example/spanishgrammarapp/GrammarVideoActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/GrammarVideoActivity.java
@@ -8,13 +8,18 @@ import android.widget.TextView;
  * Created by janaldoustorres on 06/03/2016.
  */
 public class GrammarVideoActivity extends AppCompatActivity {
+    private String subtopicTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grammar_video);
 
+        subtopicTitle = getIntent().getStringExtra(TopicActivity.SUBTOPIC);
+
         TextView tvSubtopicTitle = (TextView) findViewById(R.id.tv_grammar_video_subtopic);
-        tvSubtopicTitle.setText("Grammar Video");
+        tvSubtopicTitle.setText(subtopicTitle);
+
+
     }
 }
