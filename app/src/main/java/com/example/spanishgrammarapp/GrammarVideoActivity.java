db8abbfa8125c8831175f1328941bd0c8439aff1
janaldous <jat.torres@gmail.com>
2016-03-06 15:23:35 +0000

    added GrammarVideoActivity

---
+++ app/src/main/java/com/example/spanishgrammarapp/GrammarVideoActivity.java
@@ -0,0 +1,16 @@
+package com.example.spanishgrammarapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by janaldoustorres on 06/03/2016.
+ */
+public class GrammarVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grammar_video);
+    }
+}
--- app/src/main/java/com/example/spanishgrammarapp/TopicActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/TopicActivity.java
@@ -4,14 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class TopicActivity extends AppCompatActivity {
+public class TopicActivity extends AppCompatActivity implements OnClickListener {
     public final static String CURRENT_TOPIC = "CURRENT_TOPIC";
+    public final static String SUBTOPIC = "SUBTOPIC";
     private String currentTopic;
 
     @Override
@@ -44,6 +46,16 @@ public class TopicActivity extends AppCompatActivity {
     public void enterSituationalVideo(View view){
         Intent intent = new Intent(this, SituationalVideoActivity.class);
         intent.putExtra(CURRENT_TOPIC, currentTopic);
+        startActivity(intent);
+    }
+    
+    @Override
+    public void onClick(View v) {
+        String subtopicTitle = ((TextView) v).getText().toString();
+
+        Intent intent = new Intent(this, GrammarVideoActivity.class);
+        intent.putExtra(CURRENT_TOPIC, currentTopic);
+        intent.putExtra(SUBTOPIC, subtopicTitle);
         startActivity(intent);
     }
 }
