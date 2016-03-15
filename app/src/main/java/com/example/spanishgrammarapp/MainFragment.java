1b8f50cc785690754bfa76ea4466df28d2aea2d7
endrjs <endrjs@gmail.com>
2016-03-15 12:46:51 +0000

    after Log-in go to the .Language Activity

--- app/src/main/java/com/example/spanishgrammarapp/MainFragment.java
+++ app/src/main/java/com/example/spanishgrammarapp/MainFragment.java
@@ -1,26 +1,30 @@
 package com.example.spanishgrammarapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.Profile;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+
+import org.json.JSONObject;
+
+import java.util.Date;
 
 /**
  * Created by Pc on 2016-02-04.
  */
 public class MainFragment extends Activity {
-    // Custom button
+
     private static final String TAG_CANCEL = "FACEBOOK LOGIN";
     private Button fbbutton;
     private Button hombtn;
@@ -28,18 +32,17 @@ public class MainFragment extends Activity {
     public String str_id;
     public String str_firstname;
     public String str_lastname;
-    public ContactsContract.Data data;
+    public Date data;
     public String email2;
     // Creating Facebook CallbackManager Value
     public CallbackManager callbackmanager;
-    private TextView mTextDetails;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Initialize SDK before setContentView(Layout ID)
         FacebookSdk.sdkInitialize(getApplicationContext());
-        setContentView(R.layout.fragment_layout);
+        setContentView(R.layout.fragment_main);
         callbackmanager = CallbackManager.Factory.create();
 
         // Initialize layout button
@@ -52,26 +55,58 @@ public class MainFragment extends Activity {
                 LoginManager.getInstance().registerCallback(callbackmanager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        AccessToken accessToken = loginResult.getAccessToken();
-                        Profile profile = Profile.getCurrentProfile();
-                        if (profile != null) {
-                            mTextDetails.setText("wellcome to the Team Paragon" + profile.getName());
+                        // App code
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+                                        // Application code
+                                        Log.v("LoginActivity", response.toString());
+                                    }
 
-                        }
+                                });
+
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email,gender, birthday");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                        Intent intent = new Intent(MainFragment.this, LanguageActivity.class);
+                        startActivity(intent);
+                        finish();
+
                     }
 
                     @Override
                     public void onCancel() {
+                        // not called
+                        Log.d("fb_login_sdk", "callback cancel");
                     }
 
                     @Override
                     public void onError(FacebookException e) {
+                        // not called
+                        Log.d("fb_login_sdk", "callback onError");
                     }
                 });
+            }
+        });
 
+        hombtn = (Button) findViewById(R.id.fbhome_btn);
+        hombtn.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackmanager.onActivityResult(requestCode, resultCode, data);
     }
 }
