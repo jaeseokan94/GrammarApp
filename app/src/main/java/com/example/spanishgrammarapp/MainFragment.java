d394ab2e993c4514b2c9982085a9e82ea4b65221
endrjs <endrjs@gmail.com>
2016-03-15 12:08:29 +0000

    Now Log-in is possivle!!!!
    changed Fragement activity to Activity and change some code.

--- app/src/main/java/com/example/spanishgrammarapp/MainFragment.java
+++ app/src/main/java/com/example/spanishgrammarapp/MainFragment.java
@@ -1,12 +1,10 @@
 package com.example.spanishgrammarapp;
 
-import android.app.Fragment;
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
+import android.provider.ContactsContract;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -15,72 +13,65 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 /**
- * Created by Gun on 2016-03-13.
+ * Created by Pc on 2016-02-04.
  */
-public class MainFragment extends Fragment{
-
+public class MainFragment extends Activity {
+    // Custom button
+    private static final String TAG_CANCEL = "FACEBOOK LOGIN";
+    private Button fbbutton;
+    private Button hombtn;
+    public String str_email;
+    public String str_id;
+    public String str_firstname;
+    public String str_lastname;
+    public ContactsContract.Data data;
+    public String email2;
+    // Creating Facebook CallbackManager Value
+    public CallbackManager callbackmanager;
     private TextView mTextDetails;
-    private CallbackManager mCallBackManager;
-    private FacebookCallback<LoginResult> mcallback=new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {    //성공했을때 하는거 까먹지말자
-            AccessToken accessToken = loginResult.getAccessToken();
-            Profile profile = Profile.getCurrentProfile();
-            if(profile != null){
-                mTextDetails.setText("wellcome to the Team Paragon"+profile.getName());
-            }
-
-        }
-
-        @Override
-        public void onCancel() {                            //취소햇을때
-
-        }
-
-        @Override
-        public void onError(FacebookException error) {      //에러걸릴떄
-
-        }
-    };
-
-
-    public MainFragment(){
-
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
-        mCallBackManager = CallbackManager.Factory.create();
+        // Initialize SDK before setContentView(Layout ID)
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.fragment_layout);
+        callbackmanager = CallbackManager.Factory.create();
 
-    }
+        // Initialize layout button
+        fbbutton = (Button) findViewById(R.id.login_button);
+
+        fbbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().registerCallback(callbackmanager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        AccessToken accessToken = loginResult.getAccessToken();
+                        Profile profile = Profile.getCurrentProfile();
+                        if (profile != null) {
+                            mTextDetails.setText("wellcome to the Team Paragon" + profile.getName());
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancel() {
+                    }
+
+                    @Override
+                    public void onError(FacebookException e) {
+                    }
+                });
 
 
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_layout,container,false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        LoginButton loginButton = (LoginButton)view.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
-        loginButton.setFragment(this);
-        loginButton.registerCallback(mCallBackManager,mcallback);
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mCallBackManager.onActivityResult(requestCode,resultCode,data);
+            }
+        });
     }
 }
