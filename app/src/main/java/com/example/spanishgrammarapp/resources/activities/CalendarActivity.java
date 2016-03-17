9c7337ab7a95674f3ac188fa8158f0e6111161b6
janaldous <jat.torres@gmail.com>
2016-03-17 17:54:13 +0000

    added HolidaysActivity

--- app/src/main/java/com/example/spanishgrammarapp/resources/activities/CalendarActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/resources/activities/CalendarActivity.java
@@ -2,18 +2,37 @@ package com.example.spanishgrammarapp.resources.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.spanishgrammarapp.APIWrapper;
+import com.example.spanishgrammarapp.MainActivity;
 import com.example.spanishgrammarapp.R;
+import com.example.spanishgrammarapp.ResourcesActivity;
 
 /**
  *
  */
 public class CalendarActivity extends AppCompatActivity {
+    private String resource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //TODO implement layout
         setContentView(R.layout.activity_calendar);
+
+        //Get resource name
+        resource = getIntent().getStringExtra(ResourcesActivity.RESOURCE_NAME);
+
+        //Get instructions from API
+        String instructions = APIWrapper.getInstructions(MainActivity.LANGUAGE,
+                ResourcesActivity.DIALECT, resource);
+
+        //Set up
+        TextView tvTitle = (TextView) findViewById(R.id.tv_title);
+        tvTitle.setText(resource + " - " + ResourcesActivity.DIALECT + " accent");
+
+        TextView tvInstructions = (TextView) findViewById(R.id.tv_instructions);
+        tvInstructions.setText(instructions);
     }
 }
--- app/src/main/java/com/example/spanishgrammarapp/resources/activities/HolidaysActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/resources/activities/HolidaysActivity.java
@@ -2,18 +2,40 @@ package com.example.spanishgrammarapp.resources.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.spanishgrammarapp.APIWrapper;
+import com.example.spanishgrammarapp.MainActivity;
 import com.example.spanishgrammarapp.R;
+import com.example.spanishgrammarapp.ResourcesActivity;
 
 /**
  * Created by janaldoustorres on 17/03/2016.
  */
 public class HolidaysActivity extends AppCompatActivity {
+    private String resource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //TODO implement layout
         setContentView(R.layout.activity_holidays);
+
+        //Get resource name
+        resource = getIntent().getStringExtra(ResourcesActivity.RESOURCE_NAME);
+
+        //Get instructions from API
+        String instructions = APIWrapper.getInstructions(MainActivity.LANGUAGE,
+                ResourcesActivity.DIALECT, resource);
+
+        //Set up
+        TextView tvTitle = (TextView) findViewById(R.id.tv_title);
+        tvTitle.setText(resource + " - " + ResourcesActivity.DIALECT + " accent");
+
+        TextView tvInstructions = (TextView) findViewById(R.id.tv_instructions);
+        tvInstructions.setText(instructions);
+
+
+
     }
 }
