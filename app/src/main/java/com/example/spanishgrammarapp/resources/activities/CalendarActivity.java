b9218020823de232ad6c9b289b9d7fc1e603cd79
endrjs <endrjs@gmail.com>
2016-03-21 13:48:26 +0000

    ButtonSetasVertical

--- app/src/main/java/com/example/spanishgrammarapp/resources/activities/CalendarActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/resources/activities/CalendarActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.spanishgrammarapp.Data.APIWrapper;
@@ -41,46 +40,63 @@ public class CalendarActivity extends AppCompatActivity {
                 ResourcesActivity.DIALECT, resource);
 
         //Set up
-        TextView dayTitle = (TextView) findViewById(R.id.day_title);
-        dayTitle.setText(resource + " - " + ResourcesActivity.DIALECT + " accent");
+        //TextView dayTitle = (TextView) findViewById(R.id.day_title);
+//        dayTitle.setText(resource + " - " + ResourcesActivity.DIALECT + " accent");
 
-        TextView dayInstructions = (TextView) findViewById(R.id.day_instructions);
-        dayInstructions.setText(instructions);
+//        TextView dayInstructions = (TextView) findViewById(R.id.day_instructions);
+//        dayInstructions.setText(instructions);
 
         APIWrapper apiWrapper = new APIWrapper(database);
 
         ArrayList<Numb> numbs = apiWrapper.getNumbs(MainActivity.currentLanguage, ResourcesActivity.DIALECT);
 
         //Dynamically create letters (with pronunciation and audioURL as tag) as views
-        LinearLayout dayLayout = (LinearLayout) findViewById(R.id.calendar_layout);
+//        LinearLayout dayLayout = (LinearLayout) findViewById(R.id.calendar_layout);
 
 
         View dayView;
         for(Numb numb: numbs) {
-            dayView = getLayoutInflater().inflate(R.layout.calendar_layout, dayLayout, false);
+//            dayView = getLayoutInflater().inflate(R.layout.calendar_layout, dayLayout, false);
+//
+//            //dayView.setBackgroundColor(Color.YELLOW);
+//            dayLayout.addView(dayView);
+//            //TextView tvDay = (TextView)dayView.findViewById(R.id.day);
+//            TextView tvDay = (TextView)dayView.findViewById(R.id.day);
 
-            //dayView.setBackgroundColor(Color.YELLOW);
-            dayLayout.addView(dayView);
-            //TextView tvDay = (TextView)dayView.findViewById(R.id.day);
-            TextView tvDay = (TextView)dayView.findViewById(R.id.day);
-            TextView tvPronunciation = (TextView)dayView.findViewById(R.id.pronunciation_guide);
+//            TextView tvPronunciation = (TextView)dayView.findViewById(R.id.pronunciation_guide);
+//
+//            tvDay.setText(numb.getNumber());
+//            tvPronunciation.setText(numb.getPronunciation());
+//            dayView.setTag(numb.getAudioURL());
 
-            tvDay.setText(numb.getNumber());
-            tvPronunciation.setText(numb.getPronunciation());
-            dayView.setTag(numb.getAudioURL());
+
+
+            Button button1 = (Button) findViewById(R.id.button1);
+            button1.setText(numb.getNumber());
+          
+
+
+            Button button2 = (Button) findViewById(R.id.button2);
+            button2.setText(numb.getNumber());
+
         }
     }
 
-    public void playSound(View view) {
-        Log.d("MainActivity", "resource sound played" + (String) view.getTag());
-        try {
-            player = new MediaPlayer();
-            player.setDataSource((String)view.getTag());
-            player.prepare();
-            player.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(getApplicationContext(), "Start the Source", Toast.LENGTH_LONG).show();
+    public void onClickListner(View view){
+
+            Log.d("MainActivity", "resource sound played" + (String) view.getTag());
+            try {
+                player = new MediaPlayer();
+                player.setDataSource((String)view.getTag());
+                player.prepare();
+                player.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(getApplicationContext(), "Start the Source", Toast.LENGTH_LONG).show();
+
+
     }
-}
+
+
+    }
