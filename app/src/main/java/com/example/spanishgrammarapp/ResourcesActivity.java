0795edfdd36dd92c452f01a5e12532c7382563de
endrjs <endrjs@gmail.com>
2016-03-22 15:37:15 +0000

    changed the manifest

--- app/src/main/java/com/example/spanishgrammarapp/ResourcesActivity.java
+++ app/src/main/java/com/example/spanishgrammarapp/ResourcesActivity.java
@@ -12,6 +12,7 @@ import com.example.spanishgrammarapp.resources.activities.CalendarActivity;
 import com.example.spanishgrammarapp.resources.activities.DayActivity;
 import com.example.spanishgrammarapp.resources.activities.DialectActivity;
 import com.example.spanishgrammarapp.resources.activities.HolidaysActivity;
+import com.example.spanishgrammarapp.resources.activities.NumberActivity;
 import com.example.spanishgrammarapp.resources.activities.SeasonsAndMonthsActivity;
 
 public class ResourcesActivity extends Activity {
@@ -41,7 +42,7 @@ public class ResourcesActivity extends Activity {
     }
 
     public void showNumbersActivity(View v){
-        Intent intent = new Intent(this, NumberActivity2.class);
+        Intent intent = new Intent(this, NumberActivity.class);
         intent.putExtra(RESOURCE_NAME, NUMBERS);
         startActivity(intent);
     }
